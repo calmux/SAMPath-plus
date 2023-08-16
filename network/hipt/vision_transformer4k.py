e52612a84e7a2a97093d91406589c21a66da1a55
calmux <calmuxoto@proton.me>
2023-08-16 18:26:33 +0000

    updated travis script

---
+++ network/hipt/vision_transformer4k.py
@@ -0,0 +1,40 @@
+import argparse
+import os
+import sys
+import datetime
+import time
+import math
+import json
+from pathlib import Path
+
+import numpy as np
+from PIL import Image
+import torch
+import torch.nn as nn
+import torch.distributed as dist
+import torch.backends.cudnn as cudnn
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+from torchvision import models as torchvision_models
+
+# import utils
+# from . import vision_transformer as vits
+
+import math
+from functools import partial
+
+import torch
+import torch.nn as nn
+
+#from utils import trunc_normal_
+
+
+
+def _no_grad_trunc_normal_(tensor, mean, std, a, b):
+    # Cut & paste from PyTorch official master until it's in a few official releases - RW
+    # Method based on https://people.sc.fsu.edu/~jburkardt/presentations/truncated_normal.pdf
+    def norm_cdf(x):
+        # Computes standard normal cumulative distribution function
+        return (1. + math.erf(x / math.sqrt(2.))) / 2.
+
+    if (mean < 
